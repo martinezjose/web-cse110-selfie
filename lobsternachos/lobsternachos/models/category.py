1c6f9a38baa67815001296fec5f527a52eaceee6
Kyle Chan <kylechan@Kyles-MacBook-Pro.local>
2014-05-18 12:32:35 -0700

    Deleted models folder, added gitignore

--- lobsternachos/lobsternachos/models/category.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.ext import ndb
-
-class Category(ndb.Model):
-    name = ndb.StringProperty()
-    lastUpdated = ndb.DateTimeProperty(auto_now_add=True)
-
-    @classmethod
-    def get_key_from_name(cls, guestbook_name=None):
-        return ndb.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
--- lobsternachos/lobsternachos/models/tablet.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.ext import ndb
-
-
-class Tablet(ndb.Model):
-    macAdress = ndb.StringProperty()
-
-    @classmethod
-    def get_key_from_name(cls, guestbook_name=None):
-        return ndb.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
